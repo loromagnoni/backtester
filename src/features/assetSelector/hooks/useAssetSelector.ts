b557e4efa354b65c17676ceb1021d8ba248f14e1
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2022-10-26 22:35:35 +0200

    feat: remove closed market hours

--- src/features/assetSelector/hooks/useAssetSelector.ts
+++ src/features/assetSelector/hooks/useAssetSelector.ts
@@ -1,8 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
-import { useLogDepChange } from '../../../core/debug/useLogDepChange';
 import { CandleStickSerieData } from '../../chart';
 import { AssetData } from '../data/assets';
+import { onlyOpenMarketHours } from '../helper/onlyOpenMarketHours';
 import { useSelectedAssetSerie } from './useSelectedAssetSerie';
 
 const dateConverter = (d: string): number => {
@@ -11,13 +11,14 @@ const dateConverter = (d: string): number => {
         d.match(regex)!
     );
     return Math.round(
-        new Date(+year, +month, +day, +hour, +minute, +second).getTime() / 1000
+        new Date(+year, +month - 1, +day, +hour, +minute, +second).getTime() /
+            1000
     );
 };
 
 const chartAdapter = (data: any): CandleStickSerieData => {
     return data.map((item: any) => ({
-        time: dateConverter(item['Local time']),
+        time: dateConverter(item['Gmt time']),
         open: item.Open,
         high: item.High,
         low: item.Low,
@@ -29,7 +30,8 @@ const assetQuery = (selectedAsset?: AssetData) => async () => {
     if (selectedAsset) {
         const response = await fetch(selectedAsset.url);
         const data = chartAdapter(await response.json());
-        return data;
+        const filtered = data.filter(onlyOpenMarketHours);
+        return filtered;
     } else return [];
 };
 
