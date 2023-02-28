275f967472ec05ea2b27f4de5b6df29b546e4360
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2023-02-28 23:15:32 +0100

    fix

--- src/app/domain/asset/useChangeAsset.ts
+++ src/app/domain/asset/useChangeAsset.ts
@@ -3,20 +3,23 @@ import Asset from 'domain/models/asset';
 import changeAsset from 'domain/useCase/asset/changeAsset';
 import { useCallback } from 'react';
 import useResetChart from '../chart/useResetChart';
+import useReplayDate from '../replay/date/useReplayDate';
 import useIsAssetDataAvailable from './useIsAssetDataAvailable';
 
 export default function useChangeAsset() {
   const { stateSetter } = useDependencies();
   const resetChart = useResetChart();
   const isAssetDataAvailable = useIsAssetDataAvailable();
+  const replayDate = useReplayDate();
   return useCallback(
     (newAssetSelected: Asset) =>
       changeAsset({
         stateSetter,
         newAssetSelected,
+        replayDate,
         resetChart,
         isAssetDataAvailable,
       }),
-    [stateSetter, isAssetDataAvailable, resetChart]
+    [stateSetter, isAssetDataAvailable, replayDate, resetChart]
   );
 }
--- src/domain/useCase/asset/changeAsset.ts
+++ src/domain/useCase/asset/changeAsset.ts
@@ -1,20 +1,23 @@
 import { StateSetter } from 'domain/dependencies/state/setter';
 import Asset from 'domain/models/asset';
+import { IsAssetDataAvailableUseCase } from './isAssetDataAvailable';
 
 interface ChangeAssetDependencies {
   stateSetter: StateSetter;
   newAssetSelected: Asset;
   resetChart: CallableFunction;
-  isAssetDataAvailable: CallableFunction;
+  replayDate: Date;
+  isAssetDataAvailable: IsAssetDataAvailableUseCase;
 }
 
 export default async function changeAsset({
   stateSetter,
   newAssetSelected,
   resetChart,
+  replayDate,
   isAssetDataAvailable,
 }: ChangeAssetDependencies) {
-  if (isAssetDataAvailable()) {
+  if (await isAssetDataAvailable(newAssetSelected, replayDate)) {
     stateSetter({ assetSelected: newAssetSelected });
     resetChart();
   }
